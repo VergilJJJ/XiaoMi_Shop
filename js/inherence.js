65916343ce822470a628a23a9356f71a37cab2fa
papa <916314935@qq.com>
2019-08-01 11:12:29 +0800

    完成html顶部div

---
+++ js/inherence.js
@@ -0,0 +1,15 @@
+$(function () {
+    function CreatInhereHtml() { };
+    CreatInhereHtml.prototype = {
+        constructor: CreatInhereHtml,
+
+
+        creatSiteHtml: function () {
+            //创建头部标签
+        }
+
+
+
+
+    }
+})
