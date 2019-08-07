2ae41ac2d0b88eba294880e8df2a4bb8fcd95b2e
papa <916314935@qq.com>
2019-08-07 14:48:27 +0800

    header部分的吸顶.完成

--- js/tpl.js
+++ js/tpl.js
@@ -58,16 +58,50 @@ $(function () {
 
     };
 
+    // hover事件
+    function hover() {
+        $(".m-clauses-con").on("mouseenter", $(".site-item-nav"), function () {
+            $(this).css("height", "100px").css("opacity", "1");
+        })
+    }
+
+
+    // ======================header部分事件
+    //监听滑动判断
+    function keepHeader() {
+        $(document).scroll(
+            function () {
+                checkScroll();
+            }
+        );
+    }
+
+    //根据scrollTop来判断是否吸顶
+    function checkScroll() {
+        var top = $(window).scrollTop();
+        if (top >= 300) {
+            console.log("大于300");
+            $(".m-header").addClass("ceilingType");
+
+            // ================预留
+            // A但滑动距离>300px nav会出现并且可以调用nav
+            $(".nav-part").css("opacity", "1");
+
+        } else if (top < 300) {
+            $(".nav-part").css("opacity", "0");
+        }
+    }
+
+
+
 
     // =================================top部分的js代码=============================
     function RunTop() {
         checkLoging();
         clearUser();
-
+        hover();
     }
     RunTop();
-
-
-
-
+    keepHeader()
+    checkScroll()
 })
