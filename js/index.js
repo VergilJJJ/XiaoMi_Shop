a25d4ac87b4b0b3c402a5c9780584f95578596a5
papa <916314935@qq.com>
2019-08-10 11:02:28 +0800

    完成首页的推荐的网络请求,从数据库传到html

--- js/index.js
+++ js/index.js
@@ -1,42 +1,78 @@
 $(function () {
+    // =================专属推荐==================
     $.ajax({
         type: "get",
         url: "./server/index.php",
         // dataType: "json",
         success(res) {
             var arr = JSON.parse(res)
-            console.log(arr[0]);
+            // console.log(arr[14]);
+            // let BOX = $(".m-product-list");
             for (var i = 0; i < arr.length; i++) {
-                var name = arr[i][1];
-                var pirce = arr[i][2];
-                var color = arr[i][3];
-                var imgText = arr[i][4];
-                var imgUrl = arr[i][5];
-                var labelA = arr[i][6];
-                var labelB = arr[i][7];
-                var labelC = arr[i][8];
-                var labelD = arr[i][9];
+                // i = 14
+                var o = {};
+                o.name = arr[i][1];
+                o.price = arr[i][2];
+                o.color = arr[i][3];
+                o.imgText = arr[i][4];
+                o.imgUrl = arr[i][5];
+                o.labelA = arr[i][6];
+                o.labelB = arr[i][7];
+                o.labelC = arr[i][8];
+                o.labelD = arr[i][9];
+
+                var div = $("<div>");
+                $(div).addClass("m-goods-item-container first pro-item-category");
+
+                var divA = $("<div>");
+                $(divA).addClass("category-img-container");
+                var imgurl = $("<img>");
+                $(imgurl).attr("src", o.imgUrl);
+                var imgtext = $("<p>");
+                $(imgtext).addClass("pro-desc").html(o.imgText);
+                $(divA).append($(imgurl)).append($(imgtext));
+
+                var divB = $("<div>");
+                if (o.color != "") {
+                    $(divB).addClass("m-goods-pro-tag-con").html(o.color);
+                }
+
+                var divC = $("<div>");
+                $(divC).addClass("category-box");
+                var divC1 = $("<div>").addClass("m-goods-common-tag-con");
+
+
+                if (o.labelA != "") {
+                    $("<span>").addClass("common-tag common-tag-text").css("background-color", "rgb(217, 107, 108)").html(o.labelA).appendTo(divC1);
+                }
+                if (o.labelB != "") {
+                    $("<span>").addClass("common-tag common-tag-text").css("background-color", "rgb(217, 107, 108)").html(o.labelB).appendTo(divC1);
+                }
+                if (o.labelC != "") {
+                    $("<span>").addClass("common-tag common-tag-text").css("background-color", "rgb(217, 107, 108)").html(o.labelC).appendTo(divC1);
+                }
+                if (o.labelD != "") {
+                    $("<span>").addClass("common-tag common-tag-text").css("background-color", "rgb(217, 107, 108)").html(o.labelD).appendTo(divC1);
+                }
+
+                $(divC1).appendTo($(divC));
+
+                $("<p>").addClass("pro-info").attr("title", o.name).html(o.name).appendTo($(divC));
+
+                var Pc = $("<p>").addClass("pro-price");
+                $("<span>").addClass("pro-unit").html("¥").appendTo($(Pc));
+                $("<span>").addClass("m-num").html(o.price).appendTo($(Pc));
+                $("<span>").addClass("pro-flag").html("起").appendTo($(Pc));
+                $(Pc).appendTo($(divC));
+
+
+
+                $(div).append($(divA)).append($(divB)).append($(divC));
+
+                $(".m-product-list").append($(div));
             }
 
-            var strHtml = `
-            <div class="m-goods-item-container first pro-item-category">
-                            <div class="category-img-container">
-                                <img src="https://img.youpin.mi-img.com/800_pic/7154b457fa9590c01df659617c4eab48.png?t=webp" alt="">
-                                <p class="pro-desc">3挡风速可调，左右60°摇头，27...</p>
-                            </div>
-                            <div class="m-goods-pro-tag-con">多色可选</div>
-                            <div class="category-box">
-                                <div class="m-goods-common-tag-con">
-                                    <span class="common-tag common-tag-text" style="background-color: rgb(217, 107, 108);">特价</span>
-                                    <span class="common-tag common-tag-text" style="background-color: rgb(217, 107, 108);">加价购</span>
-                                </div>
-                                <p class="pro-info" title="SOLOVE素乐台式风扇">SOLOVE素乐台式风扇</p>
-                                <p class="pro-price">
-                                    <span class="pro-unit">¥</span>
-                                    <span class="m-num">95</span>
-                                </p>
-                            </div>
-                        </div>`;
+
 
         }
     })
